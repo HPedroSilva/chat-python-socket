ac7615443bcacaec8451a9e65196fd98f8d10deb
Pedro Henrique <pedrohs21@outlook.com>
2022-07-11 20:44:02 -0300

    Versão com criptografia mas sem multiplos clientes funcionando.

--- client.py
+++ client.py
@@ -1,14 +1,118 @@
+import time
 import socket
-SERVER = "127.0.0.1"
-PORT = 8080
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
-client.sendall(bytes("This is from Client",'UTF-8'))
+import threading
+import hashlib
+import itertools
+import json
+import sys
+from Crypto import Random
+import Crypto.Cipher.AES as AES
+import Crypto.Cipher.PKCS1_OAEP as PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+#animating loading
+done = False
+def animate():
+    for c in itertools.cycle(['....','.......','..........','............']):
+        if done:
+            break
+        sys.stdout.write('\rCONFIRMING CONNECTION TO SERVER '+c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+#public key and private key
+random_generator = Random.new().read
+key = RSA.generate(1024,random_generator)
+public = key.publickey().exportKey()
+private = key.exportKey()
+key_cipher =  PKCS1_OAEP.new(key)
+
+#hashing the public key
+hash_object = hashlib.sha1(public)
+hex_digest = hash_object.hexdigest()
+
+#Setting up socket
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#host and port input user
+#host = input("Server Address To Be Connected -> ")
+#port = int(input("Port of The Server -> "))
+host = "127.0.0.1"
+port = 80
+#binding the address and port
+server.connect((host, port))
+# printing "Server Started Message"
+thread_load = threading.Thread(target=animate)
+thread_load.start()
+
+time.sleep(4)
+done = True
+
+def send(t,name,key):
+    mess = input(name + " : ")
+    #key = key[:16]
+    #merging the message and the name
+    whole = name+" : "+mess
+    en_send = AES.new(key,AES.MODE_CFB)
+    eMsg = en_send.encrypt(whole.encode())
+    data = {"msg": eMsg.decode("latin-1"), "iv": en_send.iv.decode("latin-1")}
+    #converting the encrypted message to HEXADECIMAL to readable
+    #eMsg = eMsg.encode("hex").upper()
+    if eMsg:
+        print ("ENCRYPTED MESSAGE TO SERVER-> "+str(eMsg))
+        print ("IV TO SERVER-> "+str(en_send.iv))
+    server.send(json.dumps(data).encode())
+    
+def recv(t,key):
+    data_str = server.recv(1024)
+    data = json.loads(data_str.decode())
+    newmess = data["msg"].encode("latin-1")
+    iv = data["iv"].encode("latin-1")
+
+    print ("\nENCRYPTED MESSAGE FROM SERVER-> " + str(newmess))
+    print ("\nIV FROM SERVER -> "+str(iv))
+    #key = key[:16]
+    #decoded = newmess.decode("hex")
+    en_recv = AES.new(key,AES.MODE_CFB,iv)
+    dMsg = en_recv.decrypt(newmess)
+    print ("\n**New Message From Server**  " + time.ctime(time.time()) + " : " + str(dMsg) + "\n")
+
 while True:
-    in_data =  client.recv(1024)
-    print("From Server :" ,in_data.decode())
-    out_data = input()
-    client.sendall(bytes(out_data,'UTF-8'))
-    if out_data=='bye':
+    server.send(public)
+    confirm = server.recv(1024)
+    confirm = confirm.decode()
+    if confirm == "YES":
+        print(f"Confirmação do servidor: {hex_digest}")
+        server.send(hex_digest.encode())
+    else:
         break
-client.close()
+
+    #connected msg
+    msg = server.recv(1024)
+    en = msg
+    
+    decrypt = key_cipher.decrypt(en)
+    # hashing sha1
+    en_object = hashlib.sha1(decrypt)
+    en_digest = en_object.hexdigest()
+
+    print ("\n-----ENCRYPTED PUBLIC KEY AND SESSION KEY FROM SERVER-----")
+    print (msg)
+    print ("\n-----DECRYPTED SESSION KEY-----")
+    print (decrypt)
+    print ("\n-----HANDSHAKE COMPLETE-----\n")
+    alais = input("\nYour Name -> ")
+
+    while True:
+            
+        thread_send = threading.Thread(target=send,args=("------Sending Message------",alais,decrypt))
+        thread_recv = threading.Thread(target=recv,args=("------Recieving Message------",decrypt))
+        thread_send.start()
+        thread_recv.start()
+
+        thread_send.join()
+        thread_recv.join()
+        time.sleep(0.5)
+    time.sleep(60)
+    server.close()
+server.close()
--- server.py
+++ server.py
@@ -1,37 +1,123 @@
-import socket, threading
+import socket
+import hashlib
+import os
+import time
+import itertools
+import threading
+import sys
+import json
+import Crypto.Cipher.AES as AES
+import Crypto.Cipher.PKCS1_OAEP as PKCS1_OAEP
+from Crypto.PublicKey import RSA
 
-class ClientThread(threading.Thread):
-    def __init__(self, clientId, clientAddress, clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        self.clientId = clientId
-        print ("New connection added: ", clientAddress)
-    def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        msg = ''
-        while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg=='bye':
-                break
-            print (f"From client {self.clientId}: {msg}")
-            self.csocket.send(bytes(msg,'UTF-8'))
-        print ("Client at ", clientAddress , " disconnected...")
+#server address and port number input from admin
+# host= input("Server Address - > ")
+# port = int(input("Port - > "))
+host = "127.0.0.1"
+port = 80
+#boolean for checking server and port
+check = False
+done = False
 
+def animate():
+    for c in itertools.cycle(['....','.......','..........','............']):
+        if done:
+            break
+        sys.stdout.write('\rCHECKING IP ADDRESS AND NOT USED PORT '+c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\r -----SERVER STARTED. WAITING FOR CLIENT-----\n')
+try:
+    #setting up socket
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((host,port))
+    server.listen(5)
+    check = True
+except BaseException:
+    print ("-----Check Server Address or Port-----")
+    check = False
 
-LOCALHOST = "127.0.0.1"
-PORT = 8080
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((LOCALHOST, PORT))
-print("Server started")
-print("Waiting for client request..")
-cont = 0
+if check is True:
+    # server Quit
+    shutdown = False
+# printing "Server Started Message"
+thread_load = threading.Thread(target=animate)
+thread_load.start()
 
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(cont, clientAddress, clientsock)
-    cont += 1
-    newthread.start()
+time.sleep(4)
+done = True
+#binding client and address
+client,address = server.accept()
+print ("CLIENT IS CONNECTED. CLIENT'S ADDRESS ->",address)
+print ("\n-----WAITING FOR PUBLIC KEY & PUBLIC KEY HASH-----\n")
+
+#client's message(Public Key)
+getpbk = client.recv(2048)
+
+#conversion of string to KEY
+server_public_key = RSA.importKey(getpbk)
+key_cipher =  PKCS1_OAEP.new(server_public_key)
+
+#hashing the public key in server side for validating the hash from client
+hash_object = hashlib.sha1(getpbk)
+hex_digest = hash_object.hexdigest()
+
+if getpbk != "":
+    print (getpbk)
+    client.send("YES".encode())
+    gethash = client.recv(1024)
+    gethash = gethash.decode()
+    print ("\n-----HASH OF PUBLIC KEY----- \n"+gethash)
+if hex_digest == gethash:
+    # creating session key
+    key_128 = os.urandom(16)
+    #encrypt CTR MODE session key
+    en = AES.new(key_128,AES.MODE_CFB)
+    encrypto = en.encrypt(key_128)
+    #hashing sha1
+    en_object = hashlib.sha1(encrypto)
+    en_digest = en_object.hexdigest()
+
+    print ("\n-----SESSION KEY-----\n"+str(key_128))
+
+    #encrypting session key and public key
+    E = key_cipher.encrypt(key_128)
+    print ("\n-----ENCRYPTED PUBLIC KEY AND SESSION KEY-----\n"+str(E))
+    
+    print ("\n-----HANDSHAKE COMPLETE-----")
+    client.send(E)
+    while True:
+        #message from client
+        data_str = client.recv(1024)
+        data = json.loads(data_str.decode())
+        newmess = data["msg"].encode("latin-1")
+        iv = data["iv"].encode("latin-1")
+        #decoding the message from HEXADECIMAL to decrypt the ecrypted version of the message only
+        #decoded = newmess.decode("hex")
+        #making en_digest(session_key) as the key
+        key = en_digest[:16]
+        print ("\nENCRYPTED MESSAGE FROM CLIENT -> "+str(newmess))
+        print ("\nIV FROM CLIENT -> "+str(iv))
+        #decrypting message from the client
+        en = AES.new(key_128,AES.MODE_CFB,iv)
+        dMsg = en.decrypt(newmess)
+        print ("\n**New Message**  "+time.ctime(time.time()) +" > "+str(dMsg)+"\n")
+        mess = input("\nMessage To Client -> ")
+        if mess != "":
+            #eMsg = eMsg.encode("hex").upper()
+            en = AES.new(key_128,AES.MODE_CFB)
+            # eMsg = en.encrypt(mess.encode())
+            # if eMsg != "":
+            #     print ("ENCRYPTED MESSAGE TO CLIENT-> " + str(eMsg))
+            # client.send(eMsg)
+            eMsg = en.encrypt(mess.encode())
+            data = {"msg": eMsg.decode("latin-1"), "iv": en.iv.decode("latin-1")}
+            #converting the encrypted message to HEXADECIMAL to readable
+            #eMsg = eMsg.encode("hex").upper()
+            if eMsg:
+                print ("ENCRYPTED MESSAGE TO CLIENT-> "+str(eMsg))
+                print ("IV TO CLIENT-> "+str(en.iv))
+            client.send(json.dumps(data).encode())
+    client.close()
+else:
+    print ("\n-----PUBLIC KEY HASH DOESNOT MATCH-----\n")
