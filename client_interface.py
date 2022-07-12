0969f6c0bc8ba57a13a95794eedee24ef5357d2d
Pedro Henrique <pedrohs21@outlook.com>
2022-07-12 19:40:10 -0300

    Primeira versão de teste da tela funcionando.

--- client_interface.py
+++ client_interface.py
@@ -1,69 +1,76 @@
-from tkinter import *
-
-class Application:
+import tkinter as tk
+class Application(tk.Tk):
     def __init__(self, master=None):
         self.fontePadrao = ("Arial", "10")
-        self.primeiroContainer = Frame(master)
+        self.primeiroContainer = tk.Frame(master)
         self.primeiroContainer["pady"] = 10
         self.primeiroContainer.pack()
 
-        self.segundoContainer = Frame(master)
+        self.segundoContainer = tk.Frame(master)
         self.segundoContainer["padx"] = 20
         self.segundoContainer.pack()
 
-        self.terceiroContainer = Frame(master)
+        self.terceiroContainer = tk.Frame(master)
         self.terceiroContainer["padx"] = 20
         self.terceiroContainer.pack()
 
-        self.quartoContainer = Frame(master)
+        self.quartoContainer = tk.Frame(master)
         self.quartoContainer["pady"] = 20
         self.quartoContainer.pack()
 
-        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
-        self.titulo["font"] = ("Arial", "10", "bold")
-        self.titulo.pack()
+        # self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
+        # self.titulo["font"] = ("Arial", "10", "bold")
+        # self.titulo.pack()
 
-        self.nomeLabel = Label(self.segundoContainer,text="Nome", font=self.fontePadrao)
-        self.nomeLabel.pack(side=LEFT)
+        # self.nomeLabel = Label(self.segundoContainer,text="Nome", font=self.fontePadrao)
+        # self.nomeLabel.pack(side=LEFT)
 
-        self.nome = Entry(self.segundoContainer)
-        self.nome["width"] = 30
-        self.nome["font"] = self.fontePadrao
-        self.nome.pack(side=LEFT)
+        # self.nome = Entry(self.segundoContainer)
+        # self.nome["width"] = 30
+        # self.nome["font"] = self.fontePadrao
+        # self.nome.pack(side=LEFT)
 
-        self.senhaLabel = Label(self.terceiroContainer, text="Senha", font=self.fontePadrao)
-        self.senhaLabel.pack(side=LEFT)
+        self.displayText = tk.Text(self.segundoContainer)
+        self.displayText["width"] = 30
+        self.displayText["font"] = self.fontePadrao
+        self.displayText.pack(side=tk.LEFT)
+        self.displayText.configure(state='disabled')
 
-        self.senha = Entry(self.terceiroContainer)
-        self.senha["width"] = 30
-        self.senha["font"] = self.fontePadrao
-        self.senha["show"] = "*"
-        self.senha.pack(side=LEFT)
+        self.senhaLabel = tk.Label(self.terceiroContainer, text="Mensagem", font=self.fontePadrao)
+        self.senhaLabel.pack(side=tk.LEFT)
 
-        self.autenticar = Button(self.quartoContainer)
-        self.autenticar["text"] = "Autenticar"
+        self.msg = tk.Entry(self.terceiroContainer)
+        self.msg["width"] = 30
+        self.msg["font"] = self.fontePadrao
+        # self.msg["show"] = "*"
+        self.msg.pack(side=tk.LEFT)
+
+        self.autenticar = tk.Button(self.quartoContainer)
+        self.autenticar["text"] = "Enviar"
         self.autenticar["font"] = ("Calibri", "8")
         self.autenticar["width"] = 12
-        self.autenticar["command"] = self.verificaSenha
+        self.autenticar["command"] = self.sendMsg
         self.autenticar.pack()
 
-        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem = tk.Label(self.quartoContainer, text="", font=self.fontePadrao)
         self.mensagem.pack()
+        super().__init__()
 
-    #Método verificar senha
-    def verificaSenha(self):
-        usuario = self.nome.get()
-        senha = self.senha.get()
-        if usuario == "usuariodevmedia" and senha == "dev":
-            self.mensagem["text"] = "Autenticado"
-        else:
-            self.mensagem["text"] = "Erro na autenticação"
+    def recvMsg(self, msg):
+        self.displayText.configure(state='normal')
+        self.displayText.insert('end', "\n" + str(msg))
+        self.displayText.configure(state='disabled')
 
+    def sendMsg(self):
+        msg = self.msg.get()
+        self.msg.delete(0, tk.END)
+        print(msg)
+    # #Método verificar senha
+    # def verificaSenha(self):
+    #     usuario = self.nome.get()
+    #     senha = self.senha.get()
+    #     if usuario == "usuariodevmedia" and senha == "dev":
+    #         self.mensagem["text"] = "Autenticado"
+    #     else:
+    #         self.mensagem["text"] = "Erro na autenticação"
 
-root = Tk()
-Application(root)
-root.mainloop()
-
-text_box.configure(state='normal')
-text_box.insert('end', 'Some Text\n')
-text_box.configure(state='disabled')
