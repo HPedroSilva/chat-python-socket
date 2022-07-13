eaca198ff467ae1af2f9ff9acaa34b348e81166b
Pedro Henrique <pedrohs21@outlook.com>
2022-07-13 15:14:57 -0300

    Transformando funções send e recv do servidor em métodos da classe clientthread.

--- server.py
+++ server.py
@@ -10,34 +10,7 @@ import Crypto.Cipher.AES as AES
 import Crypto.Cipher.PKCS1_OAEP as PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-def sendMessage(message):
-    for clientThread in clientsList:
-        en = AES.new(clientThread.key_128,AES.MODE_CFB)
-        eMsg = en.encrypt(message)
-        data = {"msg": eMsg.decode("latin-1"), "iv": en.iv.decode("latin-1")}
-        if eMsg:
-            print ("ENCRYPTED MESSAGE TO CLIENT-> "+str(eMsg))
-            print ("IV TO CLIENT-> "+str(en.iv))
-        clientThread.socketClient.sendall(json.dumps(data).encode())
 
-def recvData(socketClient, en_digest, key_128):
-    while True:
-        #message from client
-        data_str = socketClient.recv(1024)
-        data = json.loads(data_str.decode())
-        newmess = data["msg"].encode("latin-1")
-        iv = data["iv"].encode("latin-1")
-        #decoding the message from HEXADECIMAL to decrypt the ecrypted version of the message only
-        #decoded = newmess.decode("hex")
-        #making en_digest(session_key) as the key
-        key = en_digest[:16]
-        print ("\nENCRYPTED MESSAGE FROM CLIENT -> "+str(newmess))
-        print ("\nIV FROM CLIENT -> "+str(iv))
-        #decrypting message from the client
-        en = AES.new(key_128,AES.MODE_CFB,iv)
-        dMsg = en.decrypt(newmess)
-        print ("\n**New Message**  "+time.ctime(time.time()) +" > "+str(dMsg)+"\n")
-        sendMessage(dMsg)
 class ClientThread(threading.Thread):
     def __init__(self, socketClientAddress, socketClient):
         threading.Thread.__init__(self)
@@ -46,6 +19,37 @@ class ClientThread(threading.Thread):
         self.key_128 = ""
         print ("Nova conexão criada com: ", self.address)
     
+
+
+
+    def sendMessage(self, message):
+        for clientThread in clientsList:
+            en = AES.new(clientThread.key_128,AES.MODE_CFB)
+            eMsg = en.encrypt(message)
+            data = {"msg": eMsg.decode("latin-1"), "iv": en.iv.decode("latin-1")}
+            if eMsg:
+                print ("ENCRYPTED MESSAGE TO CLIENT-> "+str(eMsg))
+                print ("IV TO CLIENT-> "+str(en.iv))
+            clientThread.socketClient.sendall(json.dumps(data).encode())
+
+    def recvData(self, socketClient, en_digest, key_128):
+        while True:
+            #message from client
+            data_str = socketClient.recv(1024)
+            data = json.loads(data_str.decode())
+            newmess = data["msg"].encode("latin-1")
+            iv = data["iv"].encode("latin-1")
+            #decoding the message from HEXADECIMAL to decrypt the ecrypted version of the message only
+            #decoded = newmess.decode("hex")
+            #making en_digest(session_key) as the key
+            key = en_digest[:16]
+            print ("\nENCRYPTED MESSAGE FROM CLIENT -> "+str(newmess))
+            print ("\nIV FROM CLIENT -> "+str(iv))
+            #decrypting message from the client
+            en = AES.new(key_128,AES.MODE_CFB,iv)
+            dMsg = en.decrypt(newmess)
+            print ("\n**New Message**  "+time.ctime(time.time()) +" > "+str(dMsg)+"\n")
+            self.sendMessage(dMsg)
 
     def run(self):
         print ("CLIENT IS CONNECTED. CLIENT'S ADDRESS ->", self.address)
@@ -88,7 +92,7 @@ class ClientThread(threading.Thread):
             while True:
                 #thread_send = threading.Thread(target=sendData,args=(self. socketClient, self.key_128))
                 print("aqui")
-                thread_recv = threading.Thread(target=recvData,args=(self.socketClient, en_digest, self.key_128))
+                thread_recv = threading.Thread(target=self.recvData,args=(self.socketClient, en_digest, self.key_128))
                 #thread_send.start()
                 thread_recv.start()
                 #thread_send.join()
@@ -96,8 +100,6 @@ class ClientThread(threading.Thread):
             self.socketClient.close()
         else:
             print ("\n-----PUBLIC KEY HASH DOESNOT MATCH-----\n")
-
-
 #server address and port number input from admin
 # host= input("Server Address - > ")
 # port = int(input("Port - > "))
